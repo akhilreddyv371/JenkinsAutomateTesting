1f19a997b358285c1dfdd1f1dc2fa3c34575f3aa
akhilreddyv371 <akhilreddyv371@gmail.com>
2024-12-11 11:51:44 +0530

    changes in packeage import

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,7 +1,9 @@
 package com.example;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
--- src/test/java/com/example/GoogleSearchTest.java
+++ src/test/java/com/example/GoogleSearchTest.java
@@ -1,9 +1,9 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GoogleSearchTest {
 
